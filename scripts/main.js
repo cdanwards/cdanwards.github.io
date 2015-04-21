6bd1393ca5827e5bfe507050773df323aa44f277
Daniel Edwards <cdaniel.edwards@gmail.com>
2015-04-21 12:39:03 -0400

    fixed responsive

--- scripts/main.js
+++ scripts/main.js
@@ -1,65 +1,46 @@
 
 
-// Arrow Animation
+// Scroll Animation
 
-$(window).scroll( function(){
+$(function(){
+    var target, scroll;
 
-  var topWindow = $(window).scrollTop();
-  var topWindow = topWindow * 1.5;
-  var windowHeight = $(window).height();
-  var position = topWindow / windowHeight;
-  position = 1 - position;
-  $('.arrow-wrap').css('opacity', position);
+    $("a[href*=#]:not([href=#])").on("click", function(e) {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            target = $(this.hash);
+            target = target.length ? target : $("[id=" + this.hash.slice(1) + "]");
 
-});
+            if (target.length) {
+                if (typeof document.body.style.transitionProperty === 'string') {
+                    e.preventDefault();
 
+                    var avail = $(document).height() - $(window).height();
 
+                    scroll = target.offset().top;
 
-$(document).ready(function() {
+                    if (scroll > avail) {
+                        scroll = avail;
+                    }
 
-  function filterPath(string) {
-  return string
-    .replace(/^\//,'')
-    .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
-    .replace(/\/$/,'');
-  }
-  var locationPath = filterPath(location.pathname);
-  var scrollElem = scrollableElement('html', 'body');
-  $('a[href*=#]').each(function() {
-    var thisPath = filterPath(this.pathname) || locationPath;
-    if (  locationPath == thisPath
-    && (location.hostname == this.hostname || !this.hostname)
-    && this.hash.replace(/#/,'') ) {
-      var $target = $(this.hash), target = this.hash;
-      if (target) {
-        var targetOffset = $target.offset().top;
-        $(this).click(function(event) {
-          event.preventDefault();
-          $(scrollElem).animate({scrollTop: targetOffset}, 400, function() {
-            location.hash = target;
-          });
-        });
-      }
-    }
-  });
-
-  // use the first element that is "scrollable"
-  function scrollableElement(els) {
-    for (var i = 0, argLength = arguments.length; i <argLength; i++) {
-      var el = arguments[i],
-          $scrollElement = $(el);
-      if ($scrollElement.scrollTop()> 0) {
-        return el;
-      } else {
-        $scrollElement.scrollTop(1);
-        var isScrollable = $scrollElement.scrollTop()> 0;
-        $scrollElement.scrollTop(0);
-        if (isScrollable) {
-          return el;
+                    $("html").css({
+                        "margin-top" : ( $(window).scrollTop() - scroll ) + "px",
+                        "transition" : "1s ease-in-out"
+                    }).data("transitioning", true);
+                } else {
+                    $("html, body").animate({
+                        scrollTop: scroll
+                    }, 1000);
+                    return;
+                }
+            }
         }
-      }
-    }
-    return [];
-  }
+    });
 
+    $("html").on("transitionend webkitTransitionEnd msTransitionEnd oTransitionEnd", function (e) {
+        if (e.target == e.currentTarget && $(this).data("transitioning") === true) {
+            $(this).removeAttr("style").data("transitioning", false);
+            $("html, body").scrollTop(scroll);
+            return;
+        }
+    });
 });
